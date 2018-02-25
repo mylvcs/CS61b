947a5b9aa7ec9d11a77ca4587f0a6f73f1c34ff1
mylvcs <wmy0828@nuaa.edu.cn>
2018-02-26 03:43:46 +0800

    Done proj1a

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,3 +1,155 @@
-public class ArrayDeque {
+
+public class ArrayDeque<GType> {
+    private GType[] items;
+    private int head; // mark the head index of array
+    private int tail; // mark the tail index of array
+    private int size;
+    private int RFACTOR = 2; // 扩容 factor
+    private double USAGE = 0.25; // usage factor
+
+    /** 构造一个初始容量 8 的数组，初始有效数据成员为0,
+     * when head == tail, the array is empty */
+    public ArrayDeque() {
+        items = (GType[]) new Object[8];
+        size = 0;
+        head = 0;
+        tail = 0;
+    }
+
+    /** 改变列表容量, capacity为改变后的容量.
+     * 1. Copy the [nominated head - real head] part: the firstChunk,
+     *      and position it at the beginning of the new container
+     * 2. Copy the rest [real head - nominated tail] part, following the above part.
+     * 3. Modify the head and tail.
+     * */
+    private void resize(int capacity) {
+        GType[] newContainer = (GType[]) new Object[capacity];
+        if (head > tail){
+            int firstChunk = items.length - head;
+            System.arraycopy(items, head, newContainer, 0, firstChunk);
+            System.arraycopy(items, 0, newContainer, firstChunk, size - firstChunk);
+        } else{
+            System.arraycopy(items, head, newContainer, 0, size);
+        }
+
+        items = newContainer;
+        head = 0;
+        tail = size -1;
+    }
+
+    /** Return the usage rate */
+    public double checkUsage(){
+        return size / items.length;
+    }
+
+    /** Adds an item to the front of the Deque. */
+    public void addFirst(GType x){
+        if (size == items.length) {
+            resize(size * RFACTOR);
+        }
+
+        if (isEmpty()){
+            /** for empty array, just dial the head and tail back to the initial position */
+            head = 0;
+            items[head] = x;
+            tail = 0;
+        } else {
+            int insert = (head - 1 + items.length) % items.length;
+            items[insert] = x;
+            head = insert;
+        }
+
+        size += 1;
+    }
+
+    /** Adds an item to the back of the Deque. */
+    public void addLast(GType x){
+        if (size == items.length) {
+            resize(size * RFACTOR);
+        }
+
+        if (isEmpty()){
+            /** for empty array, just dial the head and tail back to the initial position */
+            head =0;
+            items[head] = x;
+            tail = 0;
+        } else{
+            int insert = (tail + 1)% items.length;
+            items[insert] = x;
+            tail = insert;
+        }
+
+        size += 1;
+    }
+
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty(){
+        if (size>0){
+            return false;
+        }
+        return true;
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return size;
+    }
+
+    /** Prints the items in the Deque from first to last, separated by a space */
+    public void printDeque(){
+        for (int i = 0; i<size; i++){
+            System.out.print(items[ (i + head)% items.length]);
+            System.out.print(" ");
+        }
+    }
+
+    /** Removes and returns the item at the front of the Deque.
+     * If no such item exists, returns null.O(c). */
+    public GType removeFirst(){
+        if (isEmpty()){
+            return null;
+        }
+
+        GType save = items[head];
+        items[head] = null;
+        head = (head + 1) % items.length;
+        size -= 1;
+
+        if (size >= 16 && checkUsage() < USAGE){
+            resize(size * RFACTOR);
+        }
+
+        return save;
+    }
+
+    /** Removes and returns the item at the back of the Deque.
+     * If no such item exists, returns null. O(1) */
+    public GType removeLast(){
+        if (isEmpty()){
+            return null;
+        }
+
+        GType save = items[tail];
+        items[tail] = null;
+        tail = (tail - 1 + items.length) % items.length;
+        size -= 1;
+
+        if (size >= 16 && checkUsage() < USAGE ){
+            resize(size * RFACTOR);
+        }
+
+        return save;
+    }
+
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque!
+     * Use iteration.  */
+    public GType get(int index){
+        if (index > size-1){
+            return null;
+        }
+
+        return items[(index + head)%items.length];
+    }
 
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,44 +1,138 @@
-public class LinkedListDeque{
-    private T first;
+public class LinkedListDeque<GType> {
+    private class OneNode {
+        public OneNode prev; //sentinel's forward link always points to the last element
+        public GType item;
+        public OneNode next; //sentinel's backward link always points to the first element
 
-
-    public void addFirst(T item) //Adds an item of type T to the front of the deque.
-    {
-        first = new IntNode(item, first);
-        size+= 1;
-
-    }
-    public void addLast(T item) //Adds an item of type T to the back of the deque.
-    {
-
-    }
-    public boolean isEmpty() // Returns true if deque is empty, false otherwise.
-    {
-
-    }
-    public int size() //Returns the number of items in the deque.
-    {
-
+        public OneNode(OneNode p, GType i, OneNode n) {
+            prev = p;
+            item = i;
+            next = n;
+        }
     }
 
-    public void printDeque() // Prints the items in the deque from first to last, separated by a space.
-    {
+    private OneNode sentinel;
+    private int size;
 
+    /** Creates an empty deque. */
+    public LinkedListDeque(){
+        sentinel = new OneNode(null,null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
     }
 
-    // Removes and returns the item at the front of the deque. If no such item exists, returns null.
-    public T removeFirst()
-    {
-
+    /** Creates a deque with x  */
+    public LinkedListDeque(GType x){
+        sentinel = new OneNode(null, null, null);
+        sentinel.next = new OneNode(sentinel, x,sentinel);
+        sentinel.prev = sentinel.next;
+        size = 1;
     }
 
-   // Removes and returns the item at the back of the deque. If no such item exists, returns null.
-   public T removeLast(){
-
-   }
-
-    // Gets the item at the given index
-    public T get(int index){
-
+    /** Adds an item to the front of the Deque.
+     *  O(c)  */
+    public void addFirst(GType x){
+        OneNode oldFrontNode = sentinel.next;
+        OneNode newNode = new OneNode(sentinel, x, oldFrontNode);
+        sentinel.next = newNode;
+        oldFrontNode.prev = newNode;
+        size += 1;
     }
+
+    /** Adds an item to the back of the Deque.
+     * O(c) */
+    public void addLast(GType x){
+        OneNode oldBackNode = sentinel.prev;
+        OneNode newNode = new OneNode(oldBackNode, x, sentinel);
+        sentinel.prev = newNode;
+        oldBackNode.next = newNode;
+        size += 1;
+    }
+
+    /** Returns true if deque is empty, false otherwise. */
+    public boolean isEmpty(){
+        if (size>0){
+            return false;
+        }
+        return true;
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return size;
+    }
+
+    /** Prints the items in the Deque from first to last, separated by a space */
+    public void printDeque(){
+        OneNode P = sentinel;
+        for (int i = 0; i<size; i++){
+            P = P.next;
+            System.out.print(P.item);
+            System.out.print(" ");
+        }
+    }
+
+    /** Removes and returns the item at the front of the Deque.
+     * If no such item exists, returns null.O(c). */
+    public GType removeFirst(){
+        if (isEmpty()){
+            return null;
+        }
+
+        OneNode oldFrontNode = sentinel.next;
+        sentinel.next = oldFrontNode.next;
+        oldFrontNode.next.prev = sentinel;
+        size -= 1;
+        return oldFrontNode.item;
+    }
+
+    /** Removes and returns the item at the back of the Deque.
+     * If no such item exists, returns null. O(1) */
+    public GType removeLast(){
+        if (isEmpty()){
+            return null;
+        }
+
+        OneNode oldBackNode = sentinel.prev;
+        sentinel.prev = oldBackNode.prev;
+        oldBackNode.prev.next = sentinel;
+        size -= 1;
+        return oldBackNode.item;
+    }
+
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque!
+     * Use iteration.  */
+    public GType get(int index){
+        if (index > size-1){
+            return null;
+        }
+
+        OneNode P = sentinel;
+        for (int i = 0; i<=index; i++){
+            P = P.next;
+        }
+        return P.item;
+    }
+
+    /** Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+     * If no such item exists, returns null. Must not alter the deque!
+     * Use recursion. */
+    public GType getRecursive(int index) {
+        if (index > size-1){
+            return null;
+        }
+
+        OneNode P = sentinel;
+        return getRecursive(index, P.next);
+    }
+
+    public GType getRecursive(int index, OneNode P){
+        if (index==0){
+            return P.item;
+        }
+        return getRecursive(index-1, P.next);
+    }
+
 }
